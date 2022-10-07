fc315391155018f02fd6446f0c05e002a35b5cff
Harsh9724 <harsh972438@gmail.com>
2022-10-07 19:00:51 +0530

    .

--- search.php
+++ search.php
@@ -25,6 +25,11 @@
 
         }
 
+        .brdr {
+            margin: auto;
+            margin-top: 100px;
+        }
+
         .t1 {
             text-align: right;
             padding: 10px !important;
@@ -81,90 +86,84 @@
             </div>
         </div>
     </nav>
+    <div class='table1'>
+        <a href="index.html"><button class="btn btn-primary">Back</button></a>
 
+        <?php
 
-    <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "puc";
 
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "puc";
+        $conn = mysqli_connect(
+            $servername,
+            $username,
+            $password,
+            $dbname
+        );
+        if ($conn === false) {
+            die("ERROR: Could not connect. "
+                . mysqli_connect_error());
+        }
 
-    $conn = mysqli_connect(
-        $servername,
-        $username,
-        $password,
-        $dbname
-    );
-    if ($conn === false) {
-        die("ERROR: Could not connect. "
-            . mysqli_connect_error());
-    }
+        $cnum = $_POST['cnum'];
 
-    $cnum = $_POST['cnum'];
+        $q = "SELECT * FROM details WHERE Cnum = '$cnum'";
+        $result = mysqli_query($conn, $q);
 
-    $q = "SELECT * FROM details WHERE Cnum = '$cnum'";
-    $result = mysqli_query($conn, $q);
+        //show all details
+        echo "<table class='detail'>";
 
-    //show all details
-    echo "<div class='table1'>";
-    echo "<table class='detail'>";
+        while ($row = mysqli_fetch_array($result)) {
+            echo "<tr class='row1'><th class='t1'>Full Name:</th><td>&nbsp;</td><td>" . $row['Fname'] . "</td></tr>";
+            echo "<tr class='row1'><th class='t1'>Contact:</th><td></td><td>" . $row['Contact'] . "</td></tr>";
+            echo "<tr class='row1'><th class='t1'>Email:</th><td></td><td>" . $row['Email'] . "</td></tr>";
+            echo "<tr class='row1'><th class='t1'>Car Number:</th><td></td><td>" . $row['Cnum'] . "</td></tr>";
+            echo "<tr class='row1'><th class='t1'>Car Registration:</th><td></td><td>" . $row['Creg'] . "</td></tr>";
+            echo "<tr class='row1'><th class='t1'>Fuel:</th><td></td><td>" . $row['Fuel'] . "</td></tr>";
+            echo "<tr class='row1'><th class='t1'>PUC Validity Date:</th><td></td><td>" . $row['puc'] . "</td></tr>";
+        }
 
-    while ($row = mysqli_fetch_array($result)) {
-        echo "<tr class='row1'><th class='t1'>Full Name:</th><td>&nbsp;</td><td>" . $row['Fname'] . "</td></tr>";
-        echo "<tr class='row1'><th class='t1'>Contact:</th><td></td><td>" . $row['Contact'] . "</td></tr>";
-        echo "<tr class='row1'><th class='t1'>Email:</th><td></td><td>" . $row['Email'] . "</td></tr>";
-        echo "<tr class='row1'><th class='t1'>Car Number:</th><td></td><td>" . $row['Cnum'] . "</td></tr>";
-        echo "<tr class='row1'><th class='t1'>Car Registration:</th><td></td><td>" . $row['Creg'] . "</td></tr>";
-        echo "<tr class='row1'><th class='t1'>Fuel:</th><td></td><td>" . $row['Fuel'] . "</td></tr>";
-        echo "<tr class='row1'><th class='t1'>PUC Validity Date:</th><td></td><td>" . $row['puc'] . "</td></tr>";
-    }
-    echo '
-    <tr>
-    <td></td>
-    <td></td>
-        <td>
-            <a href="index.html"><button class="btn btn-primary">Back</button></a>
-        </td>
-    </tr>';
-    echo "</table>";
+        echo "</table>";
+        echo "4"
 
-    // Close connection
-    mysqli_close($conn);
+        // Close connection
+        mysqli_close($conn);
 
-    ?>
-    <table class="brdr" border=1>
-        <tr>
-            <th class='f1'>Vehicle Type</th>
-            <th class='f1'>Percentage of CO</th>
-            <th class='f1'>Hydrocarbon measured in ppm</th>
-        </tr>
-        <tr>
-            <td class="flow f1">2 and 3 wheeled vehicles (2 or 4 stroke) that are manufactured on or before 31st March 2000</td>
-            <td class='f1'>4.5</td>
-            <td class='f1'>9000</td>
-        </tr>
-        <tr>
-            <td class="flow f1">2 and 3 wheeled vehicles (2 stroke) that are manufactured after 31st March 2000</td>
-            <td class='f1'>3.5</td>
-            <td class='f1'>6000</td>
-        </tr>
-        <tr>
-            <td class="flow f1">2 and 3 wheeled vehicles (4 stroke) that are manufactured after 31st March 2000</td>
-            <td class='f1'>3.5</td>
-            <td class='f1'>4500</td>
-        </tr>
-        <tr>
-            <td class="flow f1">4 wheeled vehicles that are manufactured as per the Pre Bharat Stage II Norms</td>
-            <td class='f1'>3</td>
-            <td class='f1'>1500</td>
-        </tr>
-        <tr>
-            <td class="flow f1">4 wheeled vehicles that are manufactured as per the Pre Bharat Stage II, Stage III or subsequent Norms</td>
-            <td class='f1'>0.5</td>
-            <td class='f1'>750</td>
-        </tr>
-    </table>
+        ?>
+        <table class="brdr" border=1>
+            <tr>
+                <th class='f1'>Vehicle Type</th>
+                <th class='f1'>Percentage of CO</th>
+                <th class='f1'>Hydrocarbon measured in ppm</th>
+            </tr>
+            <tr>
+                <td class="flow f1">2 and 3 wheeled vehicles (2 or 4 stroke) that are manufactured on or before 31st March 2000</td>
+                <td class='f1'>4.5</td>
+                <td class='f1'>9000</td>
+            </tr>
+            <tr>
+                <td class="flow f1">2 and 3 wheeled vehicles (2 stroke) that are manufactured after 31st March 2000</td>
+                <td class='f1'>3.5</td>
+                <td class='f1'>6000</td>
+            </tr>
+            <tr>
+                <td class="flow f1">2 and 3 wheeled vehicles (4 stroke) that are manufactured after 31st March 2000</td>
+                <td class='f1'>3.5</td>
+                <td class='f1'>4500</td>
+            </tr>
+            <tr>
+                <td class="flow f1">4 wheeled vehicles that are manufactured as per the Pre Bharat Stage II Norms</td>
+                <td class='f1'>3</td>
+                <td class='f1'>1500</td>
+            </tr>
+            <tr>
+                <td class="flow f1">4 wheeled vehicles that are manufactured as per the Pre Bharat Stage II, Stage III or subsequent Norms</td>
+                <td class='f1'>0.5</td>
+                <td class='f1'>750</td>
+            </tr>
+        </table>
 
     </div>
 </body>
